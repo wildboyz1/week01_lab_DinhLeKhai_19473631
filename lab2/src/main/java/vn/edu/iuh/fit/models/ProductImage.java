65378e5cd87cee6a1ec4f006adcdff1e5dc09b67
wildboyz1 <114565002+wildboyz1@users.noreply.github.com>
2023-09-20 20:05:05 +0700

    Delete lab2/src/main/java/vn/edu/iuh/fit directory

--- lab2/src/main/java/vn/edu/iuh/fit/models/ProductImage.java
+++
@@ -1,8 +0,0 @@
-package vn.edu.iuh.fit.models;
-
-public class ProductImage {
-    private long product_id;
-    private long image_id;
-    private String path;
-    private String alternative;
-}
--- lab2/src/main/java/vn/edu/iuh/fit/services/CustomerService.java
+++
@@ -1,4 +0,0 @@
-package vn.edu.iuh.fit.services;
-
-public class CustomerService {
-}
